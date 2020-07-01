2a2d8f39b752d7ee97eb5f62281b80433bb8118b
occelot <brunomorpheu@gmail.com>
2020-06-30 22:36:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var nome = "Bruno Augusto";
+var idade = 32;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade);
+// console.log(frase.replace("Japão", "Brasil")); //substitui japao pelo brasil
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
